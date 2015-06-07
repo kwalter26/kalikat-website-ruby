ddee2b613903351f4f3d790dc9ef41904e761bf9
BuildTools <unconfigured@null.spigotmc.org>
2015-06-07 01:09:58 -0500

    Updating webbackup

--- kalikat/db/schema.rb
+++ kalikat/db/schema.rb
@@ -11,21 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528024454) do
+ActiveRecord::Schema.define(version: 20150419210418) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "post",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  create_table "minecraft_users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "minecrafts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,25 +26,24 @@ ActiveRecord::Schema.define(version: 20150528024454) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",   null: false
-    t.string   "encrypted_password",     limit: 255, default: "",   null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "site_username",          limit: 255
-    t.string   "minecraft_username",     limit: 255
-    t.integer  "privileges",             limit: 4,   default: 1
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "site_username"
+    t.string   "minecraft_username"
+    t.integer  "privileges",             default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  limit: 1,   default: true
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
